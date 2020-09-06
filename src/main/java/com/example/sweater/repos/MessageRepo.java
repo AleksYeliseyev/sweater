147d29441d56ade426459bda5c2d0241022955bd
AleksYeliseyev <aleksyeliseyev@gmail.com>
2020-09-06 12:18:31 +0600

    Second commit. Add authorization.

---
+++ src/main/java/com/example/sweater/repos/MessageRepo.java
@@ -0,0 +1,11 @@
+package com.example.sweater.repos;
+
+import com.example.sweater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Integer> {
+
+    List<Message> findByTag(String tag);
+}
